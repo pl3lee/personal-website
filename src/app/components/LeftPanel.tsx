91e912f4ceb30735d6acb1a0d46c33e716210802
pl3lee <billylee123@gmail.com>
2023-06-12 15:17:51 +0800

    left panel

--- src/app/components/LeftPanel.tsx
+++ src/app/components/LeftPanel.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
 import BasicInfo from "./BasicInfo";
+import ContactInfo from "./ContactInfo";
 const LeftPanel = () => {
   return (
     <div className="flex flex-col text-left justify-between sticky">
       <BasicInfo />
       <Navbar />
+      <ContactInfo />
     </div>
   );
 };
