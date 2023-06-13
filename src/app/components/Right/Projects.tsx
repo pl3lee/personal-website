73e29047a610bf0c1a10eaaa9afcf5694e18db2c
pl3lee <billylee123@gmail.com>
2023-06-13 11:30:53 +0800

    projects

--- src/app/components/Right/Projects.tsx
+++ src/app/components/Right/Projects.tsx
@@ -1,13 +1,16 @@
+"use client";
 import projectsjson from "../../projects.json";
 import { useState } from "react";
 import Project from "./Project";
+import SmallSectionTitle from "./SmallSectionTitle";
 const Projects = () => {
   const [projects, setProjects] = useState<any>(projectsjson.projects);
   return (
     <div>
-      <ul>
+      <SmallSectionTitle title="Projects" />
+      <ul className="pl-0">
         {projects.map((project: any, index: number) => (
-          <li key={index} className="mb-8 exp">
+          <li key={index} className="mb-8 exp hover-element pl-0">
             <a href={project.url} target="_blank">
               <Project proj={project} />
             </a>
--- src/app/components/Right/RightPanel.tsx
+++ src/app/components/Right/RightPanel.tsx
@@ -1,11 +1,13 @@
 import About from "./About";
 import Experiences from "./Experiences";
+import Projects from "./Projects";
 
 const RightPanel = () => {
   return (
     <div className="flex flex-col text-left lg:pt-[6rem] lg:pb-[6rem] lg:w-1/2 lg:ml-10 lg:gap-10 w-full gap-16">
       <About />
       <Experiences />
+      <Projects />
     </div>
   );
 };
