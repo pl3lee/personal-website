d7226621caf20f172c7610b21921262e6b53592e
pl3lee <billylee123@gmail.com>
2023-06-14 18:16:28 +0800

    changed margin on skills

--- src/app/components/Right/Skill.tsx
+++ src/app/components/Right/Skill.tsx
@@ -4,7 +4,7 @@ interface SkillProps {
 
 const Skill = ({ text }: SkillProps) => {
   return (
-    <div className="text-teal-300 mr-4 bg-teal-400/10 rounded-3xl px-3 py-1 font-medium leading-5 text-center mb-5">
+    <div className="text-teal-300 mr-4 bg-teal-400/10 rounded-3xl px-3 py-1 font-medium leading-5 text-center mb-5 lg:mb-0">
       {text}
     </div>
   );
