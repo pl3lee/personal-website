e3c0f765274e2c18e2724cd139b75cbd7e247364
pl3lee <billylee123@gmail.com>
2023-07-13 16:10:19 +0800

    changed title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,10 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Billy Lee",
-  description: "Personal Website",
+  title:
+    "Billy Lee - Software and Web Developer Portfolio | University of Waterloo Student",
+  description:
+    "Discover the portfolio of Billy Lee, a talented software and web developer and student at the University of Waterloo. Explore his projects and learn more about his experience in creating innovative solutions.",
 };
 
 export default function RootLayout({
