f76916f400a13ec299870ac6baf7d7fa76f1b508
pl3lee <billylee123@gmail.com>
2023-08-23 11:00:14 +0800

    added some animations to navbar

--- src/app/components/Left/Navbar.tsx
+++ src/app/components/Left/Navbar.tsx
@@ -1,10 +1,9 @@
 "use client";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { FiArrowRight } from "react-icons/fi";
-
+import { AiOutlineRight } from "react-icons/ai";
 const Navbar = () => {
-  const [activeSection, setActiveSection] = useState("");
+  const [activeSection, setActiveSection] = useState("about");
 
   useEffect(() => {
     const handleScroll = () => {
@@ -54,78 +53,26 @@ const Navbar = () => {
   }, []);
   return (
     <nav className="lg:flex flex-col gap-3 hidden lg:mb-20">
-      {/* <Link
-        href="#about"
-        scroll={false}
-        className={
-          "nav-link" + (activeSection === "about" ? " active-section" : "")
-        }
-      >
-        <h1 className="flex flex-row gap-2 items-center">
-          About
-          <FiArrowRight
-            className={`${activeSection === "about" ? "" : "hidden"}`}
-          />
-        </h1>
-      </Link> */}
       <NavLink
         sectionTitle="About"
         sectionClassName="about"
         activeSection={activeSection}
       />
-      {/* <Link
-        href="#experiences"
-        scroll={false}
-        className={
-          "nav-link" +
-          (activeSection === "experiences" ? " active-section" : "")
-        }
-      >
-        <h1>Experiences</h1>
-      </Link> */}
       <NavLink
         sectionTitle="Experiences"
         sectionClassName="experiences"
         activeSection={activeSection}
       />
-      {/* <Link
-        href="#freelance-projects"
-        scroll={false}
-        className={
-          "nav-link" +
-          (activeSection === "freelance-projects" ? " active-section" : "")
-        }
-      >
-        <h1>Freelance Projects</h1>
-      </Link> */}
       <NavLink
         sectionTitle="Freelance Projects"
         sectionClassName="freelance-projects"
         activeSection={activeSection}
       />
-      {/* <Link
-        href="#projects"
-        scroll={false}
-        className={
-          "nav-link" + (activeSection === "projects" ? " active-section" : "")
-        }
-      >
-        <h1>Projects</h1>
-      </Link> */}
       <NavLink
         sectionTitle="Projects"
         sectionClassName="projects"
         activeSection={activeSection}
       />
-      {/* <Link
-        href="#academics"
-        scroll={false}
-        className={
-          "nav-link" + (activeSection === "academics" ? " active-section" : "")
-        }
-      >
-        <h1>Academics</h1>
-      </Link> */}
       <NavLink
         sectionTitle="Academics"
         sectionClassName="academics"
@@ -153,11 +100,17 @@ const NavLink = ({
         (activeSection === sectionClassName ? " active-section" : "")
       }
     >
-      <h1 className="flex flex-row gap-2 items-center">
-        {sectionTitle}
-        <FiArrowRight
-          className={`${activeSection === sectionClassName ? "" : "hidden"}`}
+      <h1 className="flex flex-row gap-4 items-center">
+        <AiOutlineRight
+          className={`${
+            activeSection === sectionClassName ? "opacity-100" : "opacity-0"
+          }  ${activeSection === sectionClassName ? "ml-4" : ""}  ${
+            activeSection === sectionClassName
+              ? "translate-x-2"
+              : "translate-x-0"
+          } transition-all duration-500`}
         />
+        {sectionTitle}
       </h1>
     </Link>
   );
